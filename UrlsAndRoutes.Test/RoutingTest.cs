1c504f2973d429d4403ea88d3c3dd31b36535cd0
trinakriae <trinakriae@gmail.com>
2018-03-03 19:19:34 +0100

    Added Moq and reference to MVC project to test project, create test helper methods

---
+++ UrlsAndRoutes.Test/RoutingTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web.Routing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UrlsAndRoutes.Test
+{
+    [TestClass]
+    public class RoutingTest
+    {
+        public void TestRouteMatch(string url, string controller, string action, object routeProperties = null, string httpMethod = "GET")
+        {
+            //Arrange
+            RouteCollection routes = new RouteCollection();
+            RouteConfig.RegisterRoutes(routes);
+
+            //Act - process the route
+            RouteData result = routes.GetRouteData(TestHelper.CreateHttpContext(url, httpMethod));
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.IsTrue(TestHelper.TestIncomingRouteResult(result, controller, action, routeProperties));
+        }
+    }
+}
---
+++ UrlsAndRoutes.Test/TestHelper.cs
@@ -0,0 +1,40 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Routing;
+
+namespace UrlsAndRoutes.Test
+{
+    public static class TestHelper
+    {
+        internal static HttpContextBase CreateHttpContext(string targetUrl = null, string httpMethod = "GET")
+        {
+            //create the mock request
+            Mock<HttpRequestBase> mockRequest = new Mock<HttpRequestBase>();
+
+            mockRequest.Setup(m => m.AppRelativeCurrentExecutionFilePath).Returns(targetUrl);
+            mockRequest.Setup(m => m.HttpMethod).Returns(httpMethod);
+
+            //create the mock response
+            Mock<HttpResponseBase> mockResponse = new Mock<HttpResponseBase>();
+            mockResponse.Setup(m => m.ApplyAppPathModifier(It.IsAny<string>())).Returns<string>(s => s);
+
+            //create the mock context, using the request and response
+            Mock<HttpContextBase> mockContext = new Mock<HttpContextBase>();
+            mockContext.Setup(m => m.Request).Returns(mockRequest.Object);
+            mockContext.Setup(m => m.Response).Returns(mockResponse.Object);
+
+            //return the mocked object
+            return mockContext.Object;
+        }
+
+        internal static bool TestIncomingRouteResult(RouteData result, string controller, string action, object routeProperties)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
